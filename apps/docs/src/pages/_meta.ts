fbc19bb5435f49c56549dbd4480a5db1b84057f2
Zlatko <zfedor@gmail.com>
2024-10-23 22:45:27 +0200

    upgrade docs

---
+++ apps/docs/src/pages/_meta.ts
@@ -0,0 +1,17 @@
+export default {
+  index: 'Introduction',
+  guide: {
+    title: 'Guides',
+  },
+  package: {
+    title: 'Packages',
+  },
+  '-- More': {
+    type: 'separator',
+    title: 'More',
+  },
+  about: {
+    title: 'About Nodevisor',
+    href: '/about',
+  },
+};
